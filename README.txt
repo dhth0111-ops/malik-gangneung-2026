c0264cde87da4c3f00c5e5dec44f4806bc4f985d
dhth0111-ops <dhth0111@gmail.com>
2026-07-13 20:52:40 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+압축을 풀고 5개 파일을 GitHub 저장소 최상단에 업로드하세요.
+Settings > Pages > Deploy from a branch > main / root 설정.
--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>self.skipWaiting());
+const C='malik-trip-v1',A=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
