3b977d808d95c5414277b3a4d4aebe807ef3363c
dhth0111-ops <dhth0111@gmail.com>
2026-07-14 14:25:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='malik-trip-v6-quick-20260714';
+const CACHE='malik-trip-v6-ultimate-20260714-1';
 const ASSETS=['./','./index.html','./firebase-config.js','./manifest.json','./icon-192.png','./icon-512.png','./avatar-apple.png','./avatar-kokko.png','./avatar-angel.png','./hero-sea.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
