cf6e1f04c30b7b892f6b40599f2ee139f5e7c5f9
dhth0111-ops <dhth0111@gmail.com>
2026-07-24 20:45:57 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDEMk0E7oDYCrKWRoHYRe8jEPwyim3KMUI",
-  authDomain: "malik-trip.firebaseapp.com",
-  projectId: "malik-trip",
-  storageBucket: "malik-trip.firebasestorage.app",
-  messagingSenderId: "916575536609",
-  appId: "1:916575536609:web:c67e4c653290ceace1fea0",
-  measurementId: "G-YVYPGFRGK2"
-};
