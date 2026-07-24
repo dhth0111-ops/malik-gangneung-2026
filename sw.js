bc2831567245d6bb95f24d1cdfc9c42409816326
dhth0111-ops <dhth0111@gmail.com>
2026-07-24 20:47:40 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='malik-trip-v7-0-multi-trip-20260720';
+const ASSETS=['./','./index.html','./firebase-config.js','./manifest.json','./icon-192.png','./icon-512.png','./avatar-apple.png','./avatar-kokko.png','./avatar-angel.png','./hero-sea.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
